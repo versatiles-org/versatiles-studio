6cd8cf3bf93019a42c0f6bb54eeddae7bbe13fbc
Michael Kreil <github@michael-kreil.de>
2024-01-13 23:24:52 +0100

    cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,16 @@
 <script lang="ts">
-	import Greet from './lib/Greet.svelte';
 </script>
 
 <main class="container">
-	<h1>Welcome to Tauri!</h1>
-
-	<div class="row">
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-		</a>
-		<a href="https://tauri.app" target="_blank">
-			<img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-		</a>
-		<a href="https://svelte.dev" target="_blank">
-			<img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
-		</a>
-	</div>
-
-	<p>Click on the Tauri, Vite, and Svelte logos to learn more.</p>
-
-	<div class="row">
-		<Greet />
-	</div>
+	<h1>You can do</h1>
+	<ul>
+		<li>Open/preview versatiles from local/remote</li>
+		<li>generate, improve and save style</li>
+		<li>import geojson</li>
+		<li>process/improve/compress vectortiles</li>
+		<li></li>
+	</ul>
 </main>
 
 <style>
-	.logo.vite:hover {
-		filter: drop-shadow(0 0 2em #747bff);
-	}
-
-	.logo.svelte:hover {
-		filter: drop-shadow(0 0 2em #ff3e00);
-	}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import "./styles.css";
 import App from "./App.svelte";
 
-const app = new App({
-  target: document.getElementById("app"),
-});
+const target = document.getElementById("app");
+if (!target) throw Error();
+const app = new App({ target });
 
 export default app;
